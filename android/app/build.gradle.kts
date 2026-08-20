d9877fe1f9fee78f9851aa0ca924fdcadbe13f39
alexander <alexander@gmail.com>
2026-08-20 22:33:35 +0800

    build(android): keep release application id

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -72,7 +72,6 @@ android {
                 signingConfig = signingConfigs.getByName("release")
             } else {
                 signingConfig = signingConfigs.getByName("debug")
-                applicationIdSuffix = ".dev"
             }
 
             proguardFiles(
